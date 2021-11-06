cc02a8d87488ded11003613836c500e0ea004dbe
ZZH <530153239@qq.com>
2021-11-06 17:23:09 +0800

    update

--- test/go.mod
+++ test/go.mod
@@ -2,9 +2,10 @@ module test
 
 go 1.17
 
+require github.com/gin-gonic/gin v1.7.4
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.4 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
@@ -20,4 +21,11 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+    gorm.io/driver/mysql v1.1.3 // indirect
+	gorm.io/driver/sqlite v1.2.3 // indirect
+	gorm.io/gorm v1.22.2 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+    github.com/golang/protobuf v1.5.2 // indirect
+    github.com/jinzhu/inflection v1.0.0 // indirect
+    github.com/jinzhu/now v1.1.2 // indirect
 )
--- test/user_info_ctrl.go
+++ test/user_info_ctrl.go
@@ -1,17 +1,64 @@
 package main
 
-func sign_phone(phone string){
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
+func sign_phone(phone string) bool{
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	if phone == "" {
+		return false
+	}
+	var user User
+	db.Where("phone = ?",phone).First(&user)
+	if user.Phone != phone{
+		return false //手机号未找到指定用户
+	}
+	return true
 }//手机号登录信息比对
 
-func sign_username(username string, password string){
+func sign_username(username string, password string) bool{
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	if username == "" || password == ""{
+		return false
+	}
+	var user User
+	db.Where("username = ?",username).First(&user)
+	if user.Username != username||user.Password != password{
+		return false //用户名与密码不匹配
+	}
 
+	return true
 }//用户名登录信息比对
 
 func save(username string, phone string, password string){
-
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	var user User
+	user.Username = username
+	user.Password = password
+	user.Phone = phone
+	fmt.Println(user)
+	db.Create(&user)
 }//存储新用户信息
 
-func delete(phone string){
+func delete(phone string) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	var user User
+	db.Where("phone = ?",phone).First(&user)
+	db.Delete(&user)
 
 }//删除用户
